a9bf169e597736bd270bd859c828973912c35787
orkhank <118054233+orkhank@users.noreply.github.com>
2024-12-01 16:57:07 +0300

    feat(clip4cc): add __all__ to include public members

--- src/clip4cc/__init__.py
+++ src/clip4cc/__init__.py
@@ -8,3 +8,14 @@ from .model_arrange import (
     get_text_vec,
     load_model,
 )
+
+__all__ = [
+    "Clip4CCDataLoader",
+    "JsonDataLoader",
+    "assign_model_args",
+    "eval_model",
+    "get_img_pair_vec",
+    "get_single_output",
+    "get_text_vec",
+    "load_model",
+]
