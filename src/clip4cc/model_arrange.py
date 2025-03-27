912642ad8178bcae4c5c4e6ab896bdbd45d47957
Berkay Ates <atesberkay2356@gmail.com>
2025-03-27 17:39:33 +0300

    feat(modeling): add functions to encode semantic and RGB images for RSFormer

--- src/clip4cc/model_arrange.py
+++ src/clip4cc/model_arrange.py
@@ -58,6 +58,58 @@ def get_model_args():
     )
 
 
+def encode_semantic_images_for_rsformer(
+    model: CLIP4IDC,
+    before_semantic_image: Image | Path,
+    after_semantic_image: Image | Path,
+    device: torch.device,
+) -> torch.Tensor:
+    dataset = Clip4CCDataLoader(
+        bef_img_path=before_semantic_image,
+        aft_img_path=after_semantic_image, 
+        bef_sem_img_path=before_semantic_image, 
+        aft_sem_img_path=after_semantic_image,
+    )
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+    with torch.no_grad():
+        batch = tuple(t.to(device) for t in next(iter(dataloader)))
+        (bef_image, aft_image, bef_sem_image, aft_sem_image, image_mask) = batch
+
+        image_pair = torch.cat([bef_image, aft_image], 1)
+        sem_pair = torch.cat([bef_sem_image, aft_sem_image], 1)
+
+        visual_output, semantic_output = model.get_visual_output_for_rsformer(image_pair,sem_pair, image_mask,rsformer=True)
+        
+    return semantic_output
+
+
+def encode_rgb_images_for_rsformer(
+    model: CLIP4IDC,
+    before_image: Image | Path,
+    after_image: Image | Path,
+    device: torch.device,
+) -> torch.Tensor:
+    dataset = Clip4CCDataLoader(
+        bef_img_path=before_image,
+        aft_img_path=after_image, 
+        bef_sem_img_path=before_image, 
+        aft_sem_img_path=after_image,
+    )
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+    with torch.no_grad():
+        batch = tuple(t.to(device) for t in next(iter(dataloader)))
+        (bef_image, aft_image, bef_sem_image, aft_sem_image, image_mask) = batch
+
+        image_pair = torch.cat([bef_image, aft_image], 1)
+        sem_pair = torch.cat([bef_sem_image, aft_sem_image], 1)
+
+        visual_output, semantic_output = model.get_visual_output_for_rsformer(image_pair,sem_pair, image_mask,rsformer=True)
+        
+    return visual_output
+
+
 def encode_images_for_rsformer(
     model: CLIP4IDC,
     before_image: Image | Path,
